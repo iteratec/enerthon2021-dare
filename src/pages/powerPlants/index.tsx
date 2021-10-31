6e82066bcfdb7c977c6f80ee3d3a43e71cdd0ca4
Oliver Widder <oliver.widder@gmail.com>
2021-10-31 15:17:55 +0100

    copy public

--- src/pages/powerPlants/index.tsx
+++ src/pages/powerPlants/index.tsx
@@ -3,4 +3,6 @@ import * as ReactDOM from "react-dom"
 
 import {PowerPlants} from "./PowerPlants";
 
+document.getElementById("title").innerText = "Power Plants";
+
 ReactDOM.render(<PowerPlants width={window.innerWidth} height={window.innerHeight}/>, document.getElementById("container"));
