47f52aadb14bf9ce1b02569b95e197baf2e156e2
Thomas Haefeker <thomas.haefeker@iteratec.com>
2021-11-02 14:20:35 +0100

    merged chart into tooltip

--- src/pages/mapView/MapView.tsx
+++ src/pages/mapView/MapView.tsx
@@ -1,57 +1,27 @@
 import * as React from 'react';
-import * as d3 from "d3";
 import {useEffect, useState} from "react";
-import {MapContainer, TileLayer, SVGOverlay, useMapEvents, Marker, Popup} from 'react-leaflet';
-import {drawWindmill, createWindmillDefs, windmillViewbox} from "../components/drawWindmill";
+import {MapContainer, TileLayer, useMapEvents, Marker, Popup} from 'react-leaflet';
+import {scaledIcon} from "../marker/leaflet-color-markers";
+import {PowerPlantTable} from "./PowerPlantTable";
+
+import {powerPlantData} from "./powerPlantData";
+
+import {AreaChart} from "react-charts-d3";
 
 import "./mapView.scss";
+import dayjs from "dayjs";
 
-const windmills = {
-    CSR1WIND001: {
-        lat: 41.335162,
-        long: 9.753427
-    },
-    CSR1WIND002: {
-        lat: 49.602228,
-        long: 9.61516
-    },
-    CSR1WIND008: {
-        lat: 49.059187,
-        long: 9.788426
-    },
-    CSR1WIND011: {
-        lat: 47.70819,
-        long: 7.926348
-    },
-    CSR1WIND014: {
-        lat: 48.928871,
-        long: 10.273493
-    },
+const colormap = {
+    "B01": "orange",
+    "B16": "yellow",
+    "B19": "blue"
 }
 
-const prepareWindmill = (name: string) => {
-    const svg = d3.select(`svg.${name}`)
-        .attr("viewbox", `0 0 ${windmillViewbox.width} ${windmillViewbox.height}`);
-    createWindmillDefs(svg);
-}
-
-const removeWindmill = (name: string) => {
-    d3.select(`svg.${name} svg.windmill`).remove();
-}
-
-const createWindmill = (name: string, zoom: number) => {
-    const START_SCALE = 0.1;
-    drawWindmill(`id-${name}`, `svg.${name}`, zoom > 0 ? START_SCALE * zoom/5 : START_SCALE)
-}
-
-const ZoomListener = () => {
+const ZoomListener = (zoomChanged: (zoom: number) => void) => {
     const [zoom, setZoom] = useState(-1);
 
     useEffect(() => {
-        setTimeout(() => {
-            Object.keys(windmills).forEach(name => removeWindmill(name))
-            Object.keys(windmills).forEach(name => createWindmill(name, zoom))
-        })
+        zoomChanged(zoom)
     }, [zoom])
 
     const mapEvents = useMapEvents({
@@ -64,28 +34,48 @@ const ZoomListener = () => {
     return null;
 }
 
+const chartData = (name) => {
+    const date = dayjs(windmills[name].date)
+    const values = [{
+        key: name,
+        values:
+            Object.entries(windmills[name].data).map((d) => {
+                const timestamp = date.add(15 * (+d[0] - 1), 'minute');
+                return {"x": timestamp.format('H:mm'), "y": +d[1]};
+            })
+    }];
+
+    console.log(values)
+    return values;
+}
+
+const popupContent = {
+    textAlign: "center",
+    height: "300px",
+    width: "300px",
+    marginTop: "30px"
+};
+
 export const MapView = () => {
 
-    useEffect(() => {
-        setTimeout(() => {
-            Object.keys(windmills).forEach(name => prepareWindmill(name))
-        })
-    }, [])
-
-    return <MapContainer center={[51.1657, 10.4515]} bounds={[[54.62129080028218, 3.790610177286792], [47.02321945431075, 14.842855458535878]]} scrollWheelZoom={false}>
+    return <MapContainer center={[51.1657, 10.4515]}
+                         bounds={[[54.62129080028218, 3.790610177286792], [47.02321945431075, 14.842855458535878]]}
+                         scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <ZoomListener/>
-
-        {Object.keys(windmills).map((name, i) => <SVGOverlay key={i} className={name} bounds={[[windmills[name].lat - 5, windmills[name].long - 5], [windmills[name].lat + 5, windmills[name].long + 5]]}/>)}
-
-        {Object.keys(windmills).map((name, i) => <Marker key={i} position={[windmills[name].lat, windmills[name].long]}>
+        {Object.keys(powerPlantData).map((name, i) => <Marker
+            key={i}
+            icon={scaledIcon(.5, colormap[powerPlantData[name]["Energieträger"]])}
+            position={[powerPlantData[name]["Lat"], powerPlantData[name]["Lon"]]}>
             <Popup>
-                {name}
+                <h1>{name}</h1>
+                <PowerPlantTable powerPlantData={powerPlantData[name]}/>
+                <AreaChart data={chartData(name)}/>
             </Popup>
         </Marker>)}
     </MapContainer>
 }
+
