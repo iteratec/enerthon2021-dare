12691ec91723947e792d891c7b064b41f849d023
Oliver Widder <oliver.widder@gmail.com>
2021-11-02 23:38:25 +0100

    show plan data in dashboard (2)

--- src/pages/mapView/index.tsx
+++ src/pages/mapView/index.tsx
@@ -9,13 +9,19 @@ document.getElementById("title").innerText = "Map View";
 
 const defaultDay = new Date("2021-06-01");
 
+let currentName;
+
 const showDataForPlan = (name: string, day: Date) => {
     console.log(`Popup: name=${name} / day=${day}`)
-    ReactDOM.render(<PlanDataChart name={name} date={day}/>, document.getElementById("dashboard"))
+    ReactDOM.render(<PlanDataChart name={name} date={day}/>, document.getElementById("dashboard"));
+    currentName = name;
 }
 
 const showMapOnDay = (day: Date) => {
     ReactDOM.render(<MapView day={day} popupOpenedCallback={showDataForPlan}/>, document.getElementById("map"));
+    if(currentName) {
+        showDataForPlan(currentName, day);
+    }
 }
 
 ReactDOM.render(<DareDayPicker
