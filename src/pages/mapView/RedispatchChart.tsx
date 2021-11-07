ef068888ac9c0c275293170b2bd8c3dc375bdb96
Oliver Widder <oliver.widder@gmail.com>
2021-11-07 18:14:27 +0100

    activation chart (5)

--- src/pages/mapView/RedispatchChart.tsx
+++ src/pages/mapView/RedispatchChart.tsx
@@ -19,9 +19,10 @@ interface DispatchChartProps {
     width: number;
     height: number;
     chartData?: RedispatchChartData;
+    yTitle: string;
 }
 
-export function RedispatchChart({height, width, chartData}: DispatchChartProps) {
+export function RedispatchChart({height, width, chartData, yTitle}: DispatchChartProps) {
 
     return (
         <XYChart
@@ -57,7 +58,7 @@ export function RedispatchChart({height, width, chartData}: DispatchChartProps) 
             />
             <AnimatedAxis
                 key="y-axis"
-                label="RD Requirements (MW)"
+                label={yTitle}
                 orientation="right"
                 numTicks={4}
                 animationTrajectory="center"
